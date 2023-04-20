3784d7d452b293cdf5d7f2a62a87795d53b63c3d
Mkupr <mkupr@outlook.com>
2023-04-20 08:38:36 +0300

    edit storage_vac.py

--- src/storage_vac.py
+++ src/storage_vac.py
@@ -1,20 +1,41 @@
 import os
+import json
 from src.vacancies import Vacancies
 
 
 class StorageJson:
     marked_delete = []
+    data_buffer = []
 
     def __init__(self, file_name="storage_vac.json"):
         self.id = None
         self.file_name = file_name
         self.path = os.path.join('data', self.file_name)
 
+    @staticmethod
+    def to_json(vac_obj):
+        result = {"id_vac": vac_obj.id_vac, "name_vac": vac_obj.name_vac, "created_at": vac_obj.created_at,
+                  "salary_from": vac_obj.salary_from, "salary_to": vac_obj.salary_to, "place": vac_obj.place,
+                  "url_vac": vac_obj.url_vac, "employer": vac_obj.employer, "skills": vac_obj.skills,
+                  "charge": vac_obj.charge, "salary": vac_obj.salary}
+        return result
+
     def update(self, new_data: list):
         pass
 
-    def load(self, keywords: str):
-        pass
+    def load_vac(self, keywords: str):
+
+        """
+        Функция получает данные из файла operations.json в корне проекта и отбрасывает пустые словари
+        :return: Список операций в виде списка словарей
+        """
+        result = []
+        with open(self.path, 'rt', encoding='utf-8') as data_file:
+            for item in json.loads("".join(data_file.readlines())):
+                if item:
+                    result.append(item)
+
+        return result
 
     def mark_del(self):
         pass
@@ -75,3 +96,18 @@ if __name__ == '__main__':
     vac2 = Vacancies(dict_data2)
     vac3 = Vacancies(dict_data3)
     vac4 = Vacancies(dict_data4)
+
+    # Data to be written
+    dictionary = [StorageJson.to_json(item) for item in Vacancies.all]
+
+    # Serializing json
+    json_object = json.dumps(dictionary, ensure_ascii=False, indent=4)
+    print('Данные в JSON формате')
+    print(json_object)
+
+    with open('Serializing_json.json', 'w', encoding="utf-8") as file:
+        json.dump(json_object, file, ensure_ascii=False, sort_keys=False, indent=2)
+
+    with open('Serializing_json.json', 'rt', encoding="utf-8") as file:
+        loaded_data = json.load(file)
+        print(loaded_data)
