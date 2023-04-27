b4666b7b57da060bfabc6061f4a8937926d20741
Mkupr <mkupr@outlook.com>
2023-04-27 09:11:30 +0300

    finished release storage_vac.py

--- src/storage_vac.py
+++ src/storage_vac.py
@@ -1,31 +1,43 @@
-import os
 import json
+import os
 
-
-DATA_PATH = os.path.abspath("..\data")
+DATA_PATH = os.path.abspath("../data")
 
 
 class StorageJson:
-    marked_delete = []
-    data_buffer = []
+    """Данный класс сохраняет объекты класса Vacancies в файле storage_vac.json.
+    Методы класса:
+     - преобразуют объекты класса Vacancies в JSON формат;
+     - сохраняют вакансии файл;
+     - загружают вакансии из файла (целиком или согласно данным фильтра по ключевым словам или минимальной зарплате)
+     и выводят их на экран;
+     - обновляют вакансии в файле (в файле хранятся вакансии только с оригинальными id);
+     - создают и обновляют список id вакансий для удаления из файла;
+     - удаляют помеченные вакансии из файла"""
+
+    __slots__ = ('file_name', 'path', 'marked_delete', 'data_buffer',)
 
     def __init__(self, file_name="storage_vac.json"):
-        self.id = None
-        self.file_name = file_name
-        self.path = os.path.join(DATA_PATH, self.file_name)
+        self.file_name: str = file_name
+        self.path: str = os.path.join(DATA_PATH, self.file_name)
+        self.marked_delete: list = []
+        self.data_buffer: list = []
         if not os.path.exists(self.path):
             self.del_all()
 
     @staticmethod
-    def to_dict(vac_obj):
+    def to_dict(vac_obj) -> dict:
+        """Метод преобразует объект Vacancies в JSON словарь"""
+
         result = {"id_vac": vac_obj.id_vac, "name_vac": vac_obj.name_vac, "created_at": vac_obj.created_at,
                   "salary_from": vac_obj.salary_from, "salary_to": vac_obj.salary_to, "place": vac_obj.place,
                   "url_vac": vac_obj.url_vac, "employer": vac_obj.employer, "skills": vac_obj.skills,
                   "charge": vac_obj.charge, "salary": vac_obj.salary}
         return result
 
-    def update(self, new_data: list):
-        # self.data_buffer = []
+    def update(self, new_data: list) -> None:
+        """Метод обновляет вакансии в файле"""
+
         missed = 0
         written = 0
         data_for_add = [self.to_dict(item) for item in new_data]
@@ -43,11 +55,8 @@ class StorageJson:
         print(f"Записано вакансий - {written}, пропущено (уже есть в файле) - {missed}")
 
     def load_vac(self, keywords: str = "") -> list:
+        """Метод загружает вакансии данные из файла фильтруя их по параметру keywords b возвращает их в виде списка."""
 
-        """
-        Функция получает данные из файла operations.json в корне проекта и отбрасывает пустые словари
-        :return: Список операций в виде списка словарей
-        """
         result = []
         if keywords:
             with open(self.path, 'rt', encoding='utf-8') as data_file:
@@ -67,14 +76,20 @@ class StorageJson:
 
         return result
 
-    def mark_del(self, id_vac: str):
+    def mark_del(self, id_vac: str) -> None:
+        """Метод добавляет id вакансии в список для удаления из файла"""
+
         for item in id_vac.split():
             self.marked_delete.append(item)
 
-    def clear_marks(self):
+    def clear_marks(self) -> None:
+        """Метод очищает список вакансий для удаления из файла"""
+
         self.marked_delete = []
 
-    def del_marked(self):
+    def del_marked(self) -> None:
+        """Метод удаляет вакансии из файла согласно списку id для удаления"""
+
         self.data_buffer = []
         file_data = self.load_vac()
         for vac in file_data:
@@ -85,13 +100,17 @@ class StorageJson:
             json.dump(self.data_buffer, data_file, ensure_ascii=False, sort_keys=False, indent=4)
         self.clear_marks()
 
-    def del_all(self):
+    def del_all(self) -> None:
+        """Метод удаляет все вакансии из файла"""
+
         empty_list = []
         with open(self.path, 'w', encoding='utf-8') as data_file:
             json.dump(empty_list, data_file, ensure_ascii=False, sort_keys=False, indent=4)
 
     @staticmethod
-    def display_vac(data: list):
+    def display_vac(data: list) -> None:
+        """Метод выводит список вакансий на экран"""
+
         if data:
             for v in data:
                 for key, value in v.items():
